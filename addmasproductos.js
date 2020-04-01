f7468949c736094363af5759f7c9fe4e8f7c8b5f
Alexander Calderon <calderonperaza@gmail.com>
2020-04-01 12:15:00 -0600

    separando las acciones con las ordenes

---
+++ addmasproductos.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: "#appRESBAR",
+    data: {
+        // Aqui inician las propiedades que vamos a necesitar
+        //para almacenar nuestros objetos de trabajo
+
+    },
+    methods: {
+        //aqui van los metodos que vamos a necesitar
+    }
+})
+
+//recuerda que recibes un parametro: ID el cual corresponde con el id de la orden con la que vas a trabajar
