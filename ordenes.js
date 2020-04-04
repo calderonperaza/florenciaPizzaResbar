613a7b16dfc4646854501b76f68de63d6ac5e368
J3Cs <joeybrook235@gmail.com>
2020-04-04 14:13:44 -0600

    ordenar y mostrar activos funcional

--- ordenes.js
+++ ordenes.js
@@ -1,117 +1,62 @@
 new Vue({
     el: "#appRESBAR",
     data: {
-        // Aqui inician las propiedades que vamos a necesitar
-        //para almacenar nuestros objetos de trabajo
         ordenSelected: '',
-        ordenes: [{
-                id: "2345ab54c1111",
-                fecha: "2020-10-31",
-                mesero: "Juan",
-                mesa: "2",
-                cliente: "",
-                estado: "A",
-                total: 21.10,
-                observacion: "",
-
-                detalleOrden: [
-
-                    { cantidad: 1, nombre: "Hamburguesa Big", precio: 7.25, categoria: { nombre: "Platos" }, subtotal: 7.25 },
-
-                    { cantidad: 1, nombre: "Pizza Suprema", precio: 6.35, categoria: { nombre: "Platos" }, subtotal: 6.75 },
-
-                    { cantidad: 2, nombre: "Soda Fanta 12 onz", precio: 1.00, categoria: { nombre: "Bebidas" }, subtotal: 2 }
-
-                ]
-            },
-
-            {
-                id: "2345ab54c2222",
-                fecha: "2020-10-31",
-                mesero: "Luis",
-                mesa: "1",
-                cliente: "Don Carlos",
-                estado: "C",
-                total: 16.00,
-                observacion: "Sin Cebolla",
-
-                detalleOrden: [
-
-                    { cantidad: 1, nombre: "Hamburguesa Big", precio: 7.25, categoria: { nombre: "Platos" }, subtotal: 7.25 },
-
-                    { cantidad: 2, nombre: "Ensalada Cesar", precio: 5.55, categoria: { nombre: "Platos" }, subtotal: 11.10 },
-
-                    { cantidad: 1, nombre: "Soda Fanta 12 onz", precio: 1.00, categoria: { nombre: "Bebidas" }, subtotal: 1.00 },
-
-                    { cantidad: 2, nombre: "Refresco de Horchata", precio: 1.75, categoria: { nombre: "Bebidas" }, subtotal: 1.75 }
-
-                ]
-            },
-            {
-                id: "2345ab54c2222",
-                fecha: "2020-10-31",
-                mesero: "Luis",
-                mesa: "5",
-                cliente: "Don Carlos",
-                estado: "A",
-                total: 16.00,
-                observacion: "Sin Cebolla",
-
-                detalleOrden: [
-
-                    { cantidad: 1, nombre: "Hamburguesa Big", precio: 7.25, categoria: { nombre: "Platos" }, subtotal: 7.25 },
-
-                    { cantidad: 2, nombre: "Ensalada Cesar", precio: 5.55, categoria: { nombre: "Platos" }, subtotal: 11.10 },
-
-                    { cantidad: 1, nombre: "Soda Fanta 12 onz", precio: 1.00, categoria: { nombre: "Bebidas" }, subtotal: 1.00 },
-
-                    { cantidad: 2, nombre: "Refresco de Horchata", precio: 1.75, categoria: { nombre: "Bebidas" }, subtotal: 1.75 }
-
-                ]
-            }, {
-                id: "2345ab54c2222",
-                fecha: "2020-10-31",
-                mesero: "Luis",
-                mesa: "10",
-                cliente: "Don Carlos",
-                estado: "C",
-                total: 16.00,
-                observacion: "Sin Cebolla",
-
-                detalleOrden: [
-
-                    { cantidad: 1, nombre: "Hamburguesa Big", precio: 7.25, categoria: { nombre: "Platos" }, subtotal: 7.25 },
-
-                    { cantidad: 2, nombre: "Ensalada Cesar", precio: 5.55, categoria: { nombre: "Platos" }, subtotal: 11.10 },
-
-                    { cantidad: 1, nombre: "Soda Fanta 12 onz", precio: 1.00, categoria: { nombre: "Bebidas" }, subtotal: 1.00 },
-
-                    { cantidad: 2, nombre: "Refresco de Horchata", precio: 1.75, categoria: { nombre: "Bebidas" }, subtotal: 1.75 }
-
-                ]
-            }
-        ],
+        ordenes: {},
         ascendente: true,
         activos: true,
-        lactivos: [],
+        lactivos: {},
         textoBusqueda: "",
         alerta: "",
-        alertBool: true
+        alertBool: true,
+        uri:'http://localhost:3000/ordenes'
     },
     methods: {
-        //aqui van los metodos que vamos a necesitar
+        
+        obtenerOrdenes: function(){
+            axios.get(
+                this.uri+'?filter=%7B%0A%20%20%22order%22%3A%20%5B%0A%20%20%20%20%22mesa%20ASC%22%20%0A%20%20%5D%0A%7D')
+                .then(response => {
+                    this.ordenes = response.data
+                })
+                .catch(e => {console.log(e)})
+        },
+
         ordenar: function() {
-            this.ascendente = !this.ascendente
+            
             if (this.ascendente == true) {
-                this.ordenes.sort((a, b) => { return b.mesa - a.mesa })
-                this.lactivos.sort((a, b) => { return b.mesa - a.mesa })
+                axios.get(
+                    this.uri+'?filter=%7B%0A%20%20%22order%22%3A%20%5B%0A%20%20%20%20%22mesa%20ASC%22%20%0A%20%20%5D%0A%7D')
+                    .then(response => {
+                        this.ordenes = response.data
+                    })
+
+                    axios.get(
+                        this.uri+'?filter=%7B%0A%20%20%22order%22%3A%20%5B%0A%20%20%20%20%22mesa%20ASC%22%0A%20%20%5D%2C%0A%20%20%22where%22%3A%20%7B%0A%20%20%20%20%22estado%22%3A%20%22A%22%0A%20%20%7D%0A%7D')
+                        .then(response => {
+                            this.lactivos = response.data
+                        })
             } else {
-                this.ordenes.sort((a, b) => { return a.mesa - b.mesa })
-                this.lactivos.sort((a, b) => { return a.mesa - b.mesa })
+                axios.get(
+                    this.uri+'?filter=%7B%0A%20%20%22order%22%3A%20%5B%0A%20%20%20%20%22mesa%20DESC%22%0A%20%20%5D%0A%7D')
+                    .then(response => {
+                        this.ordenes = response.data
+                    })
+
+                axios.get(
+                    this.uri+'?filter=%7B%0A%20%20%22order%22%3A%20%5B%0A%20%20%20%20%22mesa%20DESC%22%0A%20%20%5D%2C%0A%20%20%22where%22%3A%20%7B%0A%20%20%20%20%22estado%22%3A%20%22A%22%0A%20%20%7D%0A%7D')
+                    .then(response => {
+                        this.lactivos = response.data
+                    })
             }
+            this.ascendente = !this.ascendente
         },
         mostrarActivos: function() {
-            this.lactivos = this.ordenes.filter(orden => orden.estado == "A")
+            axios.get(
+                this.uri+'?filter=%7B%0A%20%20%22where%22%3A%20%7B%0A%20%20%20%20%22estado%22%3A%20%22A%22%0A%20%20%7D%0A%7D')
+                .then(response => {
+                    this.lactivos = response.data
+                })
             this.activos = !this.activos
         },
         buscar: function(x) {
@@ -140,6 +85,7 @@ new Vue({
                 if (vars[0].toUpperCase() == 'ALERT') {
 
                     this.alerta = vars[1].replace(/%20/g, " ")
+                    console.log(this.alerta)
                     this.timer()
 
                 }
@@ -149,6 +95,7 @@ new Vue({
             }
 
         },
+        
 
         modificarOrden() {
             window.location = "./modificarorden.html?id=" + this.ordenSelected.id;
@@ -170,7 +117,8 @@ new Vue({
             window.location = "./addmasproductos.html?id=" + this.ordenSelected.id;
         },
     },
-    mounted() {
+    mounted(){
+        this.obtenerOrdenes()
         this.ordenar()
         this.mostrarActivos()
         this.alertLauncher()
