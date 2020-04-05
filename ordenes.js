7cb7fb488183d2f95fa40263eb73732ad5a3eadb
J3Cs <joeybrook235@gmail.com>
2020-04-04 22:17:25 -0600

    Ordenes funcional, nuevos estilos agregados

--- ordenes.js
+++ ordenes.js
@@ -12,7 +12,7 @@ new Vue({
         uri:'http://localhost:3000/ordenes'
     },
     methods: {
-        
+        //Obtiene todas las ordenes 
         obtenerOrdenes: function(){
             axios.get(this.uri)
                 .then(response => {
@@ -22,7 +22,7 @@ new Vue({
         },
 
         ordenar: function() {
-            
+            //ordena de forma ascendente las ordenes
             if (this.ascendente == true) {
                 axios.get(
                     this.uri+'?filter=%7B%0A%20%20%22order%22%3A%20%5B%0A%20%20%20%20%22mesa%20ASC%22%20%0A%20%20%5D%0A%7D')
@@ -36,6 +36,7 @@ new Vue({
                             this.lactivos = response.data
                         }).catch(e => {console.log(e)})
             } else {
+                //ordena de forma descendente las ordenes
                 axios.get(
                     this.uri+'?filter=%7B%0A%20%20%22order%22%3A%20%5B%0A%20%20%20%20%22mesa%20DESC%22%0A%20%20%5D%0A%7D')
                     .then(response => {
@@ -50,6 +51,7 @@ new Vue({
             }
             this.ascendente = !this.ascendente
         },
+        //Obtiene todas las ordenes con estado activo
         mostrarActivos: function() {
             axios.get(
                 this.uri+'?filter=%7B%0A%20%20%22where%22%3A%20%7B%0A%20%20%20%20%22estado%22%3A%20%22A%22%0A%20%20%7D%0A%7D')
@@ -58,10 +60,12 @@ new Vue({
                 }).catch(e => {console.log(e)})
             this.activos = !this.activos
         },
+        //Filtra las ordenes que contengan el texto a ingresado
         buscar: function(x) {
 
             if (this.textoBusqueda == "")
                 return true;
+            //cuando el toggle esta desactivado    
             if (this.activos) {
                 var cad = this.ordenes[x].id +
                 this.ordenes[x].cliente +
@@ -78,6 +82,7 @@ new Vue({
                 return false;
                     
             } else {
+                //cuando el toggle esta activado
                 var cad = this.lactivos[x].id +
                 this.lactivos[x].cliente +
                 this.lactivos[x].mesa +
@@ -94,6 +99,8 @@ new Vue({
             }
             
         },
+        /*Busca el mensaje pasado como parametro en la uri 
+        para mostrarlo como alert*/
         alertLauncher: function() {
             let uri = window.location.href.split('?')
             if (uri.length == 2) {
@@ -111,12 +118,10 @@ new Vue({
             }
 
         },
-        
-
         modificarOrden() {
             window.location = "./modificarorden.html?id=" + this.ordenSelected.id;
         },
-        
+        //Hace desaparecer el alert
         timer: function() {
             window.setTimeout(function() {
                 $(".alert").fadeTo(500, 0).slideUp(500, function() {
