fded2f8f33de7f46c81684055a725868abb109b6
majm5555 <jm17001@ues.edu.sv>
2020-04-08 13:00:55 -0600

    cobrar-orden

--- cobrarorden.js
+++ cobrarorden.js
@@ -5,97 +5,7 @@ new Vue({
         //para almacenar nuestros objetos de trabajo
 
         ordenSelected: '',
-        ordenes: [{
-                id: "2345ab54c1111",
-                fecha: "2020-10-31",
-                mesero: "Juan",
-                mesa: "2",
-                cliente: "",
-                estado: "A",
-                total: 21.10,
-                observacion: "",
-
-                detalleOrden: [
-
-                    { cantidad: 1, nombre: "Hamburguesa Big", precio: 7.25, categoria: { nombre: "Platos" }, subtotal: 7.25 },
-
-                    { cantidad: 1, nombre: "Pizza Suprema", precio: 6.35, categoria: { nombre: "Platos" }, subtotal: 6.75 },
-
-                    { cantidad: 2, nombre: "Soda Fanta 12 onz", precio: 1.00, categoria: { nombre: "Bebidas" }, subtotal: 2 }
-
-                ]
-            },
-
-            {
-                id: "2345ab54c2222",
-                fecha: "2020-10-31",
-                mesero: "Luis",
-                mesa: "1",
-                cliente: "Don Carlos",
-                estado: "C",
-                total: 16.00,
-                observacion: "Sin Cebolla",
-
-                detalleOrden: [
-
-                    { cantidad: 1, nombre: "Hamburguesa Big", precio: 7.25, categoria: { nombre: "Platos" }, subtotal: 7.25 },
-
-                    { cantidad: 2, nombre: "Ensalada Cesar", precio: 5.55, categoria: { nombre: "Platos" }, subtotal: 11.10 },
-
-                    { cantidad: 1, nombre: "Soda Fanta 12 onz", precio: 1.00, categoria: { nombre: "Bebidas" }, subtotal: 1.00 },
-
-                    { cantidad: 2, nombre: "Refresco de Horchata", precio: 1.75, categoria: { nombre: "Bebidas" }, subtotal: 1.75 }
-
-                ]
-            },
-            {
-                id: "2345ab54c2222",
-                fecha: "2020-10-31",
-                mesero: "Luis",
-                mesa: "5",
-                cliente: "Don Carlos",
-                estado: "A",
-                total: 16.00,
-                observacion: "Sin Cebolla",
-
-                detalleOrden: [
-
-                    { cantidad: 1, nombre: "Hamburguesa Big", precio: 7.25, categoria: { nombre: "Platos" }, subtotal: 7.25 },
-
-                    { cantidad: 2, nombre: "Ensalada Cesar", precio: 5.55, categoria: { nombre: "Platos" }, subtotal: 11.10 },
-
-                    { cantidad: 1, nombre: "Soda Fanta 12 onz", precio: 1.00, categoria: { nombre: "Bebidas" }, subtotal: 1.00 },
-
-                    { cantidad: 2, nombre: "Refresco de Horchata", precio: 1.75, categoria: { nombre: "Bebidas" }, subtotal: 1.75 }
-
-                ]
-            }, {
-                id: "2345ab54c2222",
-                fecha: "2020-10-31",
-                mesero: "Luis",
-                mesa: "10",
-                cliente: "Don Carlos",
-                estado: "C",
-                total: 16.00,
-                observacion: "Sin Cebolla",
-
-                detalleOrden: [
-
-                    { cantidad: 1, nombre: "Hamburguesa Big", precio: 7.25, categoria: { nombre: "Platos" }, subtotal: 7.25 },
-
-                    { cantidad: 2, nombre: "Ensalada Cesar", precio: 5.55, categoria: { nombre: "Platos" }, subtotal: 11.10 },
-
-                    { cantidad: 1, nombre: "Soda Fanta 12 onz", precio: 1.00, categoria: { nombre: "Bebidas" }, subtotal: 1.00 },
-
-                    { cantidad: 2, nombre: "Refresco de Horchata", precio: 1.75, categoria: { nombre: "Bebidas" }, subtotal: 1.75 }
-
-                ]
-            }
-        ],
-        ascendente: true,
-        activos: true,
-        lactivos: null,
-        textoBusqueda: ""
+        uri: 'http://localhost:3000/ordenes/'
     },
     mounted: function() {
         this.obtenerSelected();
@@ -115,14 +25,61 @@ new Vue({
         },
 
         obtenerSelected() {
-            this.ordenSelected = this.ordenes.find(item => {
-                console.log(location.href);
-                return item.id == this.getParameterByName("id");
-            })
+            axios.get(this.uri + this.getParameterByName("id"))
+                .then(response => {
+                    this.ordenSelected = response.data;
+                    let fecha = this.convertDate(this.ordenSelected.fecha);
+                    document.getElementById("fecha").textContent += "Fecha: " + fecha;
+                    this.checkTotal();
+                })
+                .catch(e => { console.log(e) });
         },
+
+        updateEstado() {
+            let uriId = this.uri + this.getParameterByName("id");
+            axios.put(uriId, this.ordenSelected);
+            console.log(this.ordenSelected);
+        },
+
         redireccionarAOrdenes() {
             window.location = "/ordenes.html?alert=este%20es%20un%20mensaje"
-        }
+        },
+
+        convertDate(date) {
+            let newDate = new Date(date);
+            let formattedDate = newDate.getDay() + "/" + newDate.getMonth() + "/" + newDate.getFullYear();
+            return formattedDate;
+        },
+
+
+        //Metodo para verificar que el total sea igual a la sumatoria de los subtotales del detalle de la Orden
+        checkTotal() {
+            let total1 = this.ordenSelected.total;
+            let total2 = 0;
+            for (let i = 0; i < this.ordenSelected.detalleOrden.length; i++) {
+                total2 += this.ordenSelected.detalleOrden[i].subtotal;
+            }
+            if (total1 != total2) {
+                this.ordenSelected.total = total2;
+            }
+        },
+
+        cobrar: function() {
+            let efectivo = document.getElementById("lblEfectivo").value;
+            if (efectivo < this.ordenSelected.total) {
+                document.getElementById("lblEfectivo").classList.add('is-invalid');
+                document.getElementById("alerta").textContent += "Ingrese suficiente efectivo";
+            } else {
+                document.getElementById("lblEfectivo").classList.remove('is-invalid');
+                document.getElementById("lblEfectivo").classList.add('is-valid');
+                document.getElementById("alerta").textContent = "";
+                this.ordenSelected.estado = "C";
+                this.updateEstado(this.ordenSelected);
+                console.log(this.ordenSelected.estado);
+                let cambio = efectivo - this.ordenSelected.total;
+                window.location = "/ordenes.html?alert=Orden%20cobrada:%20" + this.ordenSelected.id + ",%20con%20un%20total%20de:%20$" + this.ordenSelected.total + ",%20efectivo%20de:%20$" + efectivo + "%20y%20cambio%20de:%20$" + cambio.toFixed(2);
+            }
+        },
 
     },
 
