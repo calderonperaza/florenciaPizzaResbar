f62002ac1ea5b56f0aa7864ee065af5de2497db8
brahhms <abrahamatteo@gmail.com>
2020-04-11 12:15:01 -0600

    se validan campos con html pattern

--- nuevaorden.js
+++ nuevaorden.js
@@ -2,14 +2,14 @@ var app = new Vue({
     el: "#appRESBAR",
     data: {
         nuevaOrden:{
-            fecha: "2020-10-31", //fecha de sistema
+            fecha: null, 
             mesero: null,
             mesa: null,
             cliente: null,
             estado: "C",
-            total: null,
+            total: 0,
             observacion: null,
-            // propiedad que contendra los productos que el usuario agregue
+            // propiedad que se llenara los productos que el usuario agregue
             // se carga atraves de getProductos() y se le agregan los campos cantidad y subtotal
             detalleOrden: [
                    //formato--> { cantidad: 1, nombre: "Hamburguesa Big", precio: 7.25, categoria: { nombre: "Platos" }, subtotal: 7.25 }
@@ -23,23 +23,34 @@ var app = new Vue({
 
     },
     created(){ 
+        this.nuevaOrden.fecha = new Date().toISOString();
         this.getCategorias();
         this.getProductos();
     },
-
     methods: {
         //decrementar cantidad del producto a valores no negativos
-        decProducto(detalleProducto){
-            if(detalleProducto.cantidad >0) {
-                detalleProducto.cantidad--;
-                detalleProducto.subtotal = detalleProducto.cantidad*detalleProducto.precio;
+        decProducto(produ){
+            if(produ.cantidad >1) {
+                produ.cantidad--;
+            }else{
+                produ.cantidad = 0;
             }
+            this.refreshTotal(produ);
         },
         //incrementar cantidad del producto 
-        incProducto(detalleProducto){
-            detalleProducto.cantidad++;
-            detalleProducto.subtotal = detalleProducto.cantidad*detalleProducto.precio;
+        incProducto(produ){
+            produ.cantidad++;
+            this.refreshTotal(produ);
         },
+        //calcula subtotal y total
+        refreshTotal(produ){
+            let oldVal = produ.subtotal;
+            let newVal = produ.cantidad*produ.precio;
+            let diff   = newVal-oldVal;
+            this.nuevaOrden.total =  Math.round((this.nuevaOrden.total+diff) * 100) / 100;
+            produ.subtotal        =  Math.round((produ.subtotal+diff) * 100) / 100;
+        },
+
         //API get methods
         getCategorias(){
             this.mensajeApi="Obteniendo Categorias...";
@@ -71,6 +82,14 @@ var app = new Vue({
             }else{
                 this.mensajeApi="";
             }
+        },
+        //event methods
+        validateForm(){
+            let form = $('.needs-validation')[0];
+
+            if ( form.checkValidity() && this.nuevaOrden.detalleOrden.length != 0) 
+                $('#resumenModal').modal();
+            form.classList.add('was-validated');
         }
     }
 
