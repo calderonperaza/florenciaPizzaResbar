d1d68006d48b0b45e5df6ca4651577347950cca6
Kevin Figueroa <kevinozmin@gmail.com>
2020-04-13 10:04:08 -0600

    cumpliendo criterios de aceptacion, CRUD con axios, ref issue #5

--- categorias.js
+++ categorias.js
@@ -1,19 +1,80 @@
 new Vue({
     el: "#appRESBAR",
     data: {
-        categorias: [
-            {id: "1238417", nombre: "Entradas"},
-            {id: "5656773", nombre: "Plato"},
-            {id: "1445667", nombre: "Postres"},
-        ],
+        categorias: [],
         categoria: {
             id: "0",
             nombre: ""
         },
         displayOption: "",
-        searchDisplay: ""
+        agg: true,
+        searchDisplay: "",
+        urlApi: 'http://localhost:3000/categorias'
     },
     methods: {
+
+        /*
+        Modifica el registro seleccionado
+        */
+        edithRegistro(){
+            axios.put(`${this.urlApi}/${this.categoria.id}`,this.categoria).then(
+                response=>{
+                    this.getAll();
+                    console.log(response.status);
+                }
+            ).catch(ex=> {console.log(ex)})
+
+        },
+
+        /*
+        creacion de nuevos registros
+        (no se pueden crear registros vacios)
+         */
+        createRegistro: function(){
+            if(this.categoria.nombre.trim()!==""){
+                this.agg=true;
+          axios.post(this.urlApi,{
+              nombre : `${this.categoria.nombre}`
+          }).then(response =>{
+              console.log(response.status);
+              this.categorias= response.data;
+              this.getAll();
+          }).catch(ex => {
+              console.log(ex)
+          });
+            }else{
+                this.agg=false;
+            }
+
+        },
+
+        /*
+        eliman registros, correspondiente al id seleccionado
+         */
+        removeRegistro: function(){
+            this.displayOption='eliminar';
+            axios.delete(`${this.urlApi}/${this.categoria.id}`).then(
+                response =>{
+                    this.getAll();
+                    console.log(response.status)}
+            ).catch(ex=>{console.log(ex)});
+
+        },
+
+        /*
+        recolecta todos los datos al hacer una peticion al api
+         */
+        getAll(){
+          axios.get(this.urlApi).then(
+              response => {
+                  this.categorias = response.data
+              }
+          ).catch(ex => {console.log(ex)})
+        },
+
+        /*
+        limpiando valores de la categoria previamente seleccionada
+         */
         clearData() {
             this.categoria = {
                 id: "0",
@@ -29,9 +90,15 @@ new Vue({
             this.categoria=cat;
         },
         filtro(valor){
-            if(this.searchDisplay=="") return true;
+            if(this.searchDisplay==="") return true;
             let array=(this.categorias[valor].id+this.categorias[valor].nombre).toUpperCase();
-            if(array.indexOf(this.searchDisplay.toUpperCase())>=0) return true; else return false;
-        }  
+            return array.indexOf(this.searchDisplay.toUpperCase()) >= 0;
+        }
+    },
+    /*
+    hook para inicializar los valores de la tabla
+     */
+    mounted(){
+        this.getAll();
     }
-})
+});
