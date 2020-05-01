c1ff02dd4e88682e08a0ce95ae2aa9421b3e364d
Kevin Figueroa <kevinozmin@gmail.com>
2020-04-30 18:33:23 -0600

    correcciones a observaciones de test con selenium

--- categorias.js
+++ categorias.js
@@ -6,23 +6,34 @@ new Vue({
             id: "0",
             nombre: ""
         },
+
+        cat: {
+            nombre: ""
+        },
         displayOption: "",
         agg: true,
         searchDisplay: "",
-        urlApi: ApiRestUrl + "/categorias"
+        urlApi: `${ApiRestUrl}/categorias`
     },
     methods: {
 
         /*
         Modifica el registro seleccionado
         */
-        edithRegistro(){
-            axios.put(`${this.urlApi}/${this.categoria.id}`,this.categoria).then(
-                response=>{
+
+        
+
+        edithRegistro() {
+        
+            axios.patch(`${this.urlApi}/${this.categoria.id}`,{
+                nombre: this.categoria.nombre
+            }).then(
+                response => {
+                    console.log(this.cat);
                     this.getAll();
                     console.log(response.status);
                 }
-            ).catch(ex=> {console.log(ex)})
+            ).catch(ex => { console.log(ex) })
 
         },
 
@@ -30,20 +41,20 @@ new Vue({
         creacion de nuevos registros
         (no se pueden crear registros vacios)
          */
-        createRegistro: function(){
-            if(this.categoria.nombre.trim()!==""){
-                this.agg=true;
-          axios.post(this.urlApi,{
-              nombre : `${this.categoria.nombre}`
-          }).then(response =>{
-              console.log(response.status);
-              this.categorias= response.data;
-              this.getAll();
-          }).catch(ex => {
-              console.log(ex)
-          });
-            }else{
-                this.agg=false;
+        createRegistro: function () {
+            if (this.categoria.nombre.trim() !== "") {
+                this.agg = true;
+                axios.post(`${this.urlApi}`, {
+                    nombre: `${this.categoria.nombre}`
+                }).then(response => {
+                    console.log(response.status);
+                    this.categorias = response.data;
+                    this.getAll();
+                }).catch(ex => {
+                    console.log(ex)
+                });
+            } else {
+                this.agg = false;
             }
 
         },
@@ -51,25 +62,26 @@ new Vue({
         /*
         eliman registros, correspondiente al id seleccionado
          */
-        removeRegistro: function(){
-            this.displayOption='eliminar';
+        removeRegistro: function () {
+            this.displayOption = 'eliminar';
             axios.delete(`${this.urlApi}/${this.categoria.id}`).then(
-                response =>{
+                response => {
                     this.getAll();
-                    console.log(response.status)}
-            ).catch(ex=>{console.log(ex)});
+                    console.log(response.status)
+                }
+            ).catch(ex => { console.log(ex) });
 
         },
 
         /*
         recolecta todos los datos al hacer una peticion al api
          */
-        getAll(){
-          axios.get(this.urlApi).then(
-              response => {
-                  this.categorias = response.data
-              }
-          ).catch(ex => {console.log(ex)})
+        getAll() {
+            axios.get(this.urlApi).then(
+                response => {
+                    this.categorias = response.data
+                }
+            ).catch(ex => { console.log(ex) })
         },
 
         /*
@@ -80,25 +92,26 @@ new Vue({
                 id: "0",
                 nombre: ""
             }
+            this.getAll();
         },
-        addMode(){
+        addMode() {
             this.clearData();
-            this.displayOption="Agregue una nueva Categoria";
+            this.displayOption = "Agregue una nueva Categoria";
         },
-        getCategoriaSelected(cat){
-            this.displayOption="Modifique la Categoria";
-            this.categoria=cat;
+        getCategoriaSelected(cat) {
+            this.displayOption = "Modifique la Categoria";
+            this.categoria = cat;
         },
-        filtro(valor){
-            if(this.searchDisplay==="") return true;
-            let array=(this.categorias[valor].id+this.categorias[valor].nombre).toUpperCase();
+        filtro(valor) {
+            if (this.searchDisplay === "") return true;
+            let array = (this.categorias[valor].id + this.categorias[valor].nombre).toUpperCase();
             return array.indexOf(this.searchDisplay.toUpperCase()) >= 0;
         }
     },
     /*
     hook para inicializar los valores de la tabla
      */
-    mounted(){
+    mounted() {
         this.getAll();
-    }
+    },
 });
